3c13dbdd5afe0a5e8d1c4211eb32f883355e0eb8
NathanHeffley <nathanheffley@gmail.com>
2016-09-30 08:31:43 -0400

    Planning to add functionality

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
-document.addEventListener("DOMContentLoaded", function() {
-  taskList = document.getElementsByClassName("item");
-
-  for(var i = 0; i < taskList.length; i++) {
-    // taskList[i].addEventListener("click", function() { this.remove(); }, false);
-    taskList[i].addEventListener("dragstart", function(event) {
-
-    });
+$(function(){
+  function prevent_default(e) {
+    e.preventDefault();
   }
 });
